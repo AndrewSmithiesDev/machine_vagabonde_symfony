b248614319bc63bf20f00494990478a53c806d4a
Andrew Smithies <andrewsmithies.dev@gmail.com>
2026-01-21 17:54:37 +0100

    HomeController routes

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -22,7 +22,31 @@ final class HomeController extends AbstractController
         ];
 
         return $this->render('pages/home.html.twig', [
-        'instaImages' => $instaImages,
-    ]);
+            'instaImages' => $instaImages,
+        ]);
+    }
+
+    #[Route('/debarrassage', name: 'debarrassage')]
+    public function debarrassage(): Response
+    {
+        return $this->render('pages/debarrassage.html.twig');
+    }
+
+    #[Route('/brocante', name: 'brocante')]
+    public function brocante(): Response
+    {
+        return $this->render('pages/brocante.html.twig');
+    }
+
+    #[Route('/surcyclage', name: 'surcyclage')]
+    public function surcyclage(): Response
+    {
+        return $this->render('pages/surcyclage.html.twig');
+    }
+
+    #[Route('/contact', name: 'contact')]
+    public function contact(): Response
+    {
+        return $this->render('pages/contact.html.twig');
     }
 }
