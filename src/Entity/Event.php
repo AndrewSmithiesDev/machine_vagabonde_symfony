fa81e785ca64da2a95b7be9591c8c009ff953dc1
Andrew Smithies <andrewsmithies.dev@gmail.com>
2026-01-27 13:39:28 +0100

    Add default image for event

--- src/Entity/Event.php
+++ src/Entity/Event.php
@@ -20,7 +20,7 @@ class Event
     #[ORM\Column(length: 255)]
     private ?string $subtitle = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 255, nullable: true)]
     private ?string $image = null;
 
     #[ORM\Column(type: Types::TEXT)]
